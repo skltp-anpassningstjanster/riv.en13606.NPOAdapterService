b82d80ff6e91989d61c1bb0e04b8d460f866d186
serp <torbjorncla@gmail.com>
2014-11-10 10:52:20 +0100

    De-lombok

--- NPOAdapterIntegrationComponent/src/main/java/se/skl/skltpservices/npoadapter/mapper/util/EHRUtil.java
+++ NPOAdapterIntegrationComponent/src/main/java/se/skl/skltpservices/npoadapter/mapper/util/EHRUtil.java
@@ -19,6 +19,7 @@
  */
 package se.skl.skltpservices.npoadapter.mapper.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -27,9 +28,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.xml.datatype.XMLGregorianCalendar;
-
-import lombok.Data;
-import lombok.SneakyThrows;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -95,8 +93,7 @@ public final class EHRUtil {
     }
 
     //
-    @SneakyThrows
-    public static Date parseTimestamp(String timestamp) {
+    public static Date parseTimestamp(String timestamp) throws ParseException {
         return formatter.get().parse(timestamp);
     }
 
@@ -607,13 +604,54 @@ public final class EHRUtil {
 
     // Generic baseline of data types to be able to convert between schemas (java packages).
     //
-    @Data
     public static class Result {
         private ResultCode resultCode;
         private ErrorCode errorCode;
         private String logId;
         private String subCode;
         private String message;
+        public Result() {
+        	super();
+        }
+		public Result(ResultCode resultCode, ErrorCode errorCode, String logId,
+				String subCode, String message) {
+			super();
+			this.resultCode = resultCode;
+			this.errorCode = errorCode;
+			this.logId = logId;
+			this.subCode = subCode;
+			this.message = message;
+		}
+		public ResultCode getResultCode() {
+			return resultCode;
+		}
+		public void setResultCode(ResultCode resultCode) {
+			this.resultCode = resultCode;
+		}
+		public ErrorCode getErrorCode() {
+			return errorCode;
+		}
+		public void setErrorCode(ErrorCode errorCode) {
+			this.errorCode = errorCode;
+		}
+		public String getLogId() {
+			return logId;
+		}
+		public void setLogId(String logId) {
+			this.logId = logId;
+		}
+		public String getSubCode() {
+			return subCode;
+		}
+		public void setSubCode(String subCode) {
+			this.subCode = subCode;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
     }
 
     //
@@ -631,10 +669,31 @@ public final class EHRUtil {
         TECHNICAL_ERROR;
     }
     
-    @Data
     public static class TimePeriod {
     	private String start;
     	private String end;
+    	public TimePeriod() {
+    		super();
+    	}
+		public TimePeriod(String start, String end) {
+			super();
+			this.start = start;
+			this.end = end;
+		}
+		public String getStart() {
+			return start;
+		}
+		public void setStart(String start) {
+			this.start = start;
+		}
+		public String getEnd() {
+			return end;
+		}
+		public void setEnd(String end) {
+			this.end = end;
+		}
+    	
+    	
     }
     
     public static class Request {
@@ -682,27 +741,84 @@ public final class EHRUtil {
     }
 
     //
-    @Data
     public static class DatePeriod {
         private String start;
         private String end;
+        public DatePeriod() {
+        	super();
+        }
+		public DatePeriod(String start, String end) {
+			super();
+			this.start = start;
+			this.end = end;
+		}
+		public String getStart() {
+			return start;
+		}
+		public void setStart(String start) {
+			this.start = start;
+		}
+		public String getEnd() {
+			return end;
+		}
+		public void setEnd(String end) {
+			this.end = end;
+		}
+        
     }
 
     //
-    @Data
     public static class PersonId {
         private String id;
         private String type;
+        public PersonId() {
+        	super();
+        }
+		public PersonId(String id, String type) {
+			super();
+			this.id = id;
+			this.type = type;
+		}
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getType() {
+			return type;
+		}
+		public void setType(String type) {
+			this.type = type;
+		}
     }
     
-    @Data
     public static class IIType {
     	private String extension;
     	private String root;
+    	public IIType() {
+    		super();
+    	}
+		public IIType(String extension, String root) {
+			super();
+			this.extension = extension;
+			this.root = root;
+		}
+		public String getExtension() {
+			return extension;
+		}
+		public void setExtension(String extension) {
+			this.extension = extension;
+		}
+		public String getRoot() {
+			return root;
+		}
+		public void setRoot(String root) {
+			this.root = root;
+		}
     }
 
     //
-    @Data
     public static class CV {
         private String code;
         private String codeSystem;
@@ -710,10 +826,59 @@ public final class EHRUtil {
         private String codeSystemVersion;
         private String displayName;
         private String originalText;
+        public CV() {
+        	super();
+        }
+		public CV(String code, String codeSystem, String codeSystemName,
+				String codeSystemVersion, String displayName,
+				String originalText) {
+			super();
+			this.code = code;
+			this.codeSystem = codeSystem;
+			this.codeSystemName = codeSystemName;
+			this.codeSystemVersion = codeSystemVersion;
+			this.displayName = displayName;
+			this.originalText = originalText;
+		}
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+		public String getCodeSystem() {
+			return codeSystem;
+		}
+		public void setCodeSystem(String codeSystem) {
+			this.codeSystem = codeSystem;
+		}
+		public String getCodeSystemName() {
+			return codeSystemName;
+		}
+		public void setCodeSystemName(String codeSystemName) {
+			this.codeSystemName = codeSystemName;
+		}
+		public String getCodeSystemVersion() {
+			return codeSystemVersion;
+		}
+		public void setCodeSystemVersion(String codeSystemVersion) {
+			this.codeSystemVersion = codeSystemVersion;
+		}
+		public String getDisplayName() {
+			return displayName;
+		}
+		public void setDisplayName(String displayName) {
+			this.displayName = displayName;
+		}
+		public String getOriginalText() {
+			return originalText;
+		}
+		public void setOriginalText(String originalText) {
+			this.originalText = originalText;
+		}
     }
 
     //
-    @Data
     public static class HealthcareProfessional {
         private String authorTime;
         private String healthcareProfessionalHSAId;
@@ -722,10 +887,74 @@ public final class EHRUtil {
         private OrgUnit healthcareProfessionalOrgUnit;
         private String healthcareProfessionalCareUnitHSAId;
         private String healthcareProfessionalCareGiverHSAId;
+        public HealthcareProfessional() {
+        	super();
+        }
+		public HealthcareProfessional(String authorTime,
+				String healthcareProfessionalHSAId,
+				String healthcareProfessionalName,
+				CV healthcareProfessionalRoleCode,
+				OrgUnit healthcareProfessionalOrgUnit,
+				String healthcareProfessionalCareUnitHSAId,
+				String healthcareProfessionalCareGiverHSAId) {
+			super();
+			this.authorTime = authorTime;
+			this.healthcareProfessionalHSAId = healthcareProfessionalHSAId;
+			this.healthcareProfessionalName = healthcareProfessionalName;
+			this.healthcareProfessionalRoleCode = healthcareProfessionalRoleCode;
+			this.healthcareProfessionalOrgUnit = healthcareProfessionalOrgUnit;
+			this.healthcareProfessionalCareUnitHSAId = healthcareProfessionalCareUnitHSAId;
+			this.healthcareProfessionalCareGiverHSAId = healthcareProfessionalCareGiverHSAId;
+		}
+		public String getAuthorTime() {
+			return authorTime;
+		}
+		public void setAuthorTime(String authorTime) {
+			this.authorTime = authorTime;
+		}
+		public String getHealthcareProfessionalHSAId() {
+			return healthcareProfessionalHSAId;
+		}
+		public void setHealthcareProfessionalHSAId(String healthcareProfessionalHSAId) {
+			this.healthcareProfessionalHSAId = healthcareProfessionalHSAId;
+		}
+		public String getHealthcareProfessionalName() {
+			return healthcareProfessionalName;
+		}
+		public void setHealthcareProfessionalName(String healthcareProfessionalName) {
+			this.healthcareProfessionalName = healthcareProfessionalName;
+		}
+		public CV getHealthcareProfessionalRoleCode() {
+			return healthcareProfessionalRoleCode;
+		}
+		public void setHealthcareProfessionalRoleCode(CV healthcareProfessionalRoleCode) {
+			this.healthcareProfessionalRoleCode = healthcareProfessionalRoleCode;
+		}
+		public OrgUnit getHealthcareProfessionalOrgUnit() {
+			return healthcareProfessionalOrgUnit;
+		}
+		public void setHealthcareProfessionalOrgUnit(
+				OrgUnit healthcareProfessionalOrgUnit) {
+			this.healthcareProfessionalOrgUnit = healthcareProfessionalOrgUnit;
+		}
+		public String getHealthcareProfessionalCareUnitHSAId() {
+			return healthcareProfessionalCareUnitHSAId;
+		}
+		public void setHealthcareProfessionalCareUnitHSAId(
+				String healthcareProfessionalCareUnitHSAId) {
+			this.healthcareProfessionalCareUnitHSAId = healthcareProfessionalCareUnitHSAId;
+		}
+		public String getHealthcareProfessionalCareGiverHSAId() {
+			return healthcareProfessionalCareGiverHSAId;
+		}
+		public void setHealthcareProfessionalCareGiverHSAId(
+				String healthcareProfessionalCareGiverHSAId) {
+			this.healthcareProfessionalCareGiverHSAId = healthcareProfessionalCareGiverHSAId;
+		}
+        
     }
 
     //
-    @Data
     public static class OrgUnit {
         private String orgUnitHSAId;
         private String orgUnitName;
@@ -733,10 +962,61 @@ public final class EHRUtil {
         private String orgUnitEmail;
         private String orgUnitAddress;
         private String orgUnitLocation;
+        public OrgUnit() {
+        	super();
+        }
+		public OrgUnit(String orgUnitHSAId, String orgUnitName,
+				String orgUnitTelecom, String orgUnitEmail,
+				String orgUnitAddress, String orgUnitLocation) {
+			super();
+			this.orgUnitHSAId = orgUnitHSAId;
+			this.orgUnitName = orgUnitName;
+			this.orgUnitTelecom = orgUnitTelecom;
+			this.orgUnitEmail = orgUnitEmail;
+			this.orgUnitAddress = orgUnitAddress;
+			this.orgUnitLocation = orgUnitLocation;
+		}
+		public String getOrgUnitHSAId() {
+			return orgUnitHSAId;
+		}
+		public void setOrgUnitHSAId(String orgUnitHSAId) {
+			this.orgUnitHSAId = orgUnitHSAId;
+		}
+		public String getOrgUnitName() {
+			return orgUnitName;
+		}
+		public void setOrgUnitName(String orgUnitName) {
+			this.orgUnitName = orgUnitName;
+		}
+		public String getOrgUnitTelecom() {
+			return orgUnitTelecom;
+		}
+		public void setOrgUnitTelecom(String orgUnitTelecom) {
+			this.orgUnitTelecom = orgUnitTelecom;
+		}
+		public String getOrgUnitEmail() {
+			return orgUnitEmail;
+		}
+		public void setOrgUnitEmail(String orgUnitEmail) {
+			this.orgUnitEmail = orgUnitEmail;
+		}
+		public String getOrgUnitAddress() {
+			return orgUnitAddress;
+		}
+		public void setOrgUnitAddress(String orgUnitAddress) {
+			this.orgUnitAddress = orgUnitAddress;
+		}
+		public String getOrgUnitLocation() {
+			return orgUnitLocation;
+		}
+		public void setOrgUnitLocation(String orgUnitLocation) {
+			this.orgUnitLocation = orgUnitLocation;
+		}
+        
+        
     }
 
     //
-    @Data
     public static class PatientSummaryHeader {
         private String documentId;
         private String sourceSystemHSAId;
@@ -749,15 +1029,144 @@ public final class EHRUtil {
         private String careContactId;
         private Boolean nullified;
         private String nullifiedReason;
+        public PatientSummaryHeader() {
+        	super();
+        }
+		public PatientSummaryHeader(String documentId,
+				String sourceSystemHSAId, String documentTitle,
+				String documentTime, PersonId patientId,
+				HealthcareProfessional accountableHealthcareProfessional,
+				LegalAuthenticator legalAuthenticator,
+				boolean approvedForPatient, String careContactId,
+				Boolean nullified, String nullifiedReason) {
+			super();
+			this.documentId = documentId;
+			this.sourceSystemHSAId = sourceSystemHSAId;
+			this.documentTitle = documentTitle;
+			this.documentTime = documentTime;
+			this.patientId = patientId;
+			this.accountableHealthcareProfessional = accountableHealthcareProfessional;
+			this.legalAuthenticator = legalAuthenticator;
+			this.approvedForPatient = approvedForPatient;
+			this.careContactId = careContactId;
+			this.nullified = nullified;
+			this.nullifiedReason = nullifiedReason;
+		}
+		public String getDocumentId() {
+			return documentId;
+		}
+		public void setDocumentId(String documentId) {
+			this.documentId = documentId;
+		}
+		public String getSourceSystemHSAId() {
+			return sourceSystemHSAId;
+		}
+		public void setSourceSystemHSAId(String sourceSystemHSAId) {
+			this.sourceSystemHSAId = sourceSystemHSAId;
+		}
+		public String getDocumentTitle() {
+			return documentTitle;
+		}
+		public void setDocumentTitle(String documentTitle) {
+			this.documentTitle = documentTitle;
+		}
+		public String getDocumentTime() {
+			return documentTime;
+		}
+		public void setDocumentTime(String documentTime) {
+			this.documentTime = documentTime;
+		}
+		public PersonId getPatientId() {
+			return patientId;
+		}
+		public void setPatientId(PersonId patientId) {
+			this.patientId = patientId;
+		}
+		public HealthcareProfessional getAccountableHealthcareProfessional() {
+			return accountableHealthcareProfessional;
+		}
+		public void setAccountableHealthcareProfessional(
+				HealthcareProfessional accountableHealthcareProfessional) {
+			this.accountableHealthcareProfessional = accountableHealthcareProfessional;
+		}
+		public LegalAuthenticator getLegalAuthenticator() {
+			return legalAuthenticator;
+		}
+		public void setLegalAuthenticator(LegalAuthenticator legalAuthenticator) {
+			this.legalAuthenticator = legalAuthenticator;
+		}
+		public boolean isApprovedForPatient() {
+			return approvedForPatient;
+		}
+		public void setApprovedForPatient(boolean approvedForPatient) {
+			this.approvedForPatient = approvedForPatient;
+		}
+		public String getCareContactId() {
+			return careContactId;
+		}
+		public void setCareContactId(String careContactId) {
+			this.careContactId = careContactId;
+		}
+		public Boolean getNullified() {
+			return nullified;
+		}
+		public void setNullified(Boolean nullified) {
+			this.nullified = nullified;
+		}
+		public String getNullifiedReason() {
+			return nullifiedReason;
+		}
+		public void setNullifiedReason(String nullifiedReason) {
+			this.nullifiedReason = nullifiedReason;
+		}
+        
+        
     }
 
     //
-    @Data
     public static class LegalAuthenticator {
         private String signatureTime;
         private String legalAuthenticatorHSAId;
         private String legalAuthenticatorName;
         private CV legalAuthenticatorRoleCode;
+        
+		public LegalAuthenticator() {
+			super();
+		}
+		public LegalAuthenticator(String signatureTime,
+				String legalAuthenticatorHSAId, String legalAuthenticatorName,
+				CV legalAuthenticatorRoleCode) {
+			super();
+			this.signatureTime = signatureTime;
+			this.legalAuthenticatorHSAId = legalAuthenticatorHSAId;
+			this.legalAuthenticatorName = legalAuthenticatorName;
+			this.legalAuthenticatorRoleCode = legalAuthenticatorRoleCode;
+		}
+		public String getSignatureTime() {
+			return signatureTime;
+		}
+		public void setSignatureTime(String signatureTime) {
+			this.signatureTime = signatureTime;
+		}
+		public String getLegalAuthenticatorHSAId() {
+			return legalAuthenticatorHSAId;
+		}
+		public void setLegalAuthenticatorHSAId(String legalAuthenticatorHSAId) {
+			this.legalAuthenticatorHSAId = legalAuthenticatorHSAId;
+		}
+		public String getLegalAuthenticatorName() {
+			return legalAuthenticatorName;
+		}
+		public void setLegalAuthenticatorName(String legalAuthenticatorName) {
+			this.legalAuthenticatorName = legalAuthenticatorName;
+		}
+		public CV getLegalAuthenticatorRoleCode() {
+			return legalAuthenticatorRoleCode;
+		}
+		public void setLegalAuthenticatorRoleCode(CV legalAuthenticatorRoleCode) {
+			this.legalAuthenticatorRoleCode = legalAuthenticatorRoleCode;
+		}
+        
     }
     
 }
