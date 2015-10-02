60442867cd077bcd7bda8cdb7e8a4bd7e0381716
Martin Flower <martin.flower@callistaenterprise.se>
2015-10-02 12:16:36 +0200

    NPO Adapter - refactor code - common functionality in header class

---
+++ NPOAdapterIntegrationComponent/src/test/java/se/skl/skltpservices/npoadapter/mapper/MapperTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2014 Inera AB, <http://inera.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package se.skl.skltpservices.npoadapter.mapper;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Scanner;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.mockito.ArgumentCaptor;
+import org.mule.api.MuleMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.skl.skltpservices.npoadapter.mapper.error.MapperException;
+
+public abstract class MapperTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MapperTest.class);
+    
+    // Use the mapper to convert the 13606 test file xml into rivta xml
+    protected String getRivtaXml(Mapper mapper, String xml13606TestFile) {
+        
+        String responseXml = "";
+        
+        // load xml from test file - this contains an <ehr_extract/>
+        StringBuilder xml13606Response = new StringBuilder();
+        try (@SuppressWarnings("resource") Scanner inputStringScanner = new Scanner(getClass().getResourceAsStream(xml13606TestFile), "UTF-8").useDelimiter("\\z")) {
+            while (inputStringScanner.hasNext()) {
+                xml13606Response.append(inputStringScanner.next());
+            }
+        }
+
+        // wrap the <ehr_extract/> in a <RIV13606REQUEST_EHR_EXTRACT_response/>
+        // opening tag
+        xml13606Response.insert("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".length(),"<RIV13606REQUEST_EHR_EXTRACT_response xmlns=\"urn:riv13606:v1.1\">");
+        // closing tag
+        xml13606Response.append("</RIV13606REQUEST_EHR_EXTRACT_response>\n");
+        
+        // pass the <RIV13606REQUEST_EHR_EXTRACT_response/> message into the ReferralOutcomeMapper - expect back a <GetReferralOutcomeResponse/>
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        Reader xmlReader = new StringReader(xml13606Response.toString());
+        XMLStreamReader xmlStreamReader;
+        try {
+            xmlStreamReader = xmlInputFactory.createXMLStreamReader(xmlReader);
+
+            MuleMessage mockMuleMessage = mock(MuleMessage.class);
+            when(mockMuleMessage.getPayload()).thenReturn(xmlStreamReader);
+            // argumentCaptor will capture the converted xml
+            ArgumentCaptor<Object> argumentCaptor = ArgumentCaptor.forClass(Object.class);
+            
+            // method being exercised on the object under test
+            mapper.mapResponse(mockMuleMessage);
+            
+            // verifications & assertions
+            verify(mockMuleMessage).setPayload(argumentCaptor.capture());
+            responseXml = (String)argumentCaptor.getValue();
+            log.debug(responseXml);
+        } catch (XMLStreamException e) {
+            fail(e.getLocalizedMessage());
+        } catch (MapperException e) {
+            fail(e.getCause().getLocalizedMessage());
+        }
+        return responseXml;
+    }
+
+}
--- NPOAdapterIntegrationComponent/src/test/java/se/skl/skltpservices/npoadapter/mapper/ReferralOutcomeMapperTest.java
+++ NPOAdapterIntegrationComponent/src/test/java/se/skl/skltpservices/npoadapter/mapper/ReferralOutcomeMapperTest.java
@@ -24,16 +24,11 @@ import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.Reader;
-import java.io.StringReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -41,33 +36,24 @@ import java.util.regex.Pattern;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.mule.api.MuleMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import riv.clinicalprocess.healthcond.actoutcome._3.ReferralOutcomeType;
 import riv.clinicalprocess.healthcond.actoutcome.enums._3.ReferralOutcomeTypeCodeEnum;
 import riv.clinicalprocess.healthcond.actoutcome.getreferraloutcomeresponder._3.GetReferralOutcomeResponseType;
 import se.rivta.en13606.ehrextract.v11.EHREXTRACT;
-import se.skl.skltpservices.npoadapter.mapper.error.MapperException;
 import se.skl.skltpservices.npoadapter.test.Util;
 
 /**
  * @author Martin Flower
  */
-public class ReferralOutcomeMapperTest {
+public class ReferralOutcomeMapperTest extends MapperTest {
 
     private static EHREXTRACT ehrextractOneResponse;
     
-    private static final Logger log = LoggerFactory.getLogger(ReferralOutcomeMapperTest.class);
-
     @BeforeClass
     public static void init() throws JAXBException {
         ehrextractOneResponse = Util.loadEhrTestData(Util.REFERRALOUTCOME_TEST_FILE_1);
@@ -125,192 +111,68 @@ public class ReferralOutcomeMapperTest {
 	}
 
 
+	
     @Test
     public void mapOneResponse() {
 
-        ReferralOutcomeMapper objectUnderTest = getReferralOutcomeMapper();
+        String responseXml = getRivtaXml(getReferralOutcomeMapper(), Util.REFERRALOUTCOME_TEST_FILE_1);
 
-        // load xml from test file - this contains an <ehr_extract/>
-        StringBuilder xml13606Response = new StringBuilder();
-        try (@SuppressWarnings("resource") Scanner inputStringScanner = new Scanner(getClass().getResourceAsStream(Util.REFERRALOUTCOME_TEST_FILE_1), "UTF-8").useDelimiter("\\z")) {
-            while (inputStringScanner.hasNext()) {
-                xml13606Response.append(inputStringScanner.next());
-            }
-        }
-
-        // wrap the <ehr_extract/> in a <RIV13606REQUEST_EHR_EXTRACT_response/>
-        // opening tag
-        xml13606Response.insert("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".length(),"<RIV13606REQUEST_EHR_EXTRACT_response xmlns=\"urn:riv13606:v1.1\">");
-        // closing tag
-        xml13606Response.append("</RIV13606REQUEST_EHR_EXTRACT_response>\n");
-        
-        // pass the <RIV13606REQUEST_EHR_EXTRACT_response/> message into the ReferralOutcomeMapper - expect back a <GetReferralOutcomeResponse/>
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-        Reader xmlReader = new StringReader(xml13606Response.toString());
-        XMLStreamReader xmlStreamReader;
-        try {
-            xmlStreamReader = xmlInputFactory.createXMLStreamReader(xmlReader);
-
-            MuleMessage mockMuleMessage = mock(MuleMessage.class);
-            when(mockMuleMessage.getPayload()).thenReturn(xmlStreamReader);
-            // argumentCaptor will capture the converted xml
-            ArgumentCaptor<Object> argumentCaptor = ArgumentCaptor.forClass(Object.class);
-
-            // method being exercised
-            objectUnderTest.mapResponse(mockMuleMessage);
-
-            
-            // verifications & assertions
-            verify(mockMuleMessage).setPayload(argumentCaptor.capture());
-            String responseXml = (String)argumentCaptor.getValue();
-            log.debug(responseXml);
-            
-            assertTrue(responseXml.contains("<GetReferralOutcomeResponse"));
-            assertTrue(responseXml.contains("sourceSystemHSAId><ns2:documentTime>20100503165801</ns2:documentTime><ns2:patientId>"));
-            assertTrue(responseXml.contains("referralOutcomeTypeCode>SS"));
-            assertTrue(responseXml.contains("referralOutcomeTitle>Allmän Remiss Vårdcentralen Strängnäs EDI"));
-            assertTrue(responseXml.contains("referralOutcomeText>Svar: test Svarsdatum: 100503 Dikterande"));
-            assertTrue(responseXml.contains("act><ns2:actCode><ns2:code>620<"));
-            assertTrue(responseXml.contains("<ns2:codeSystem>1.2.752.129.2.2.2.1<"));
-            assertTrue(responseXml.contains("<ns2:actText>Önskad undersökning: test"));
-            assertTrue(responseXml.contains("actText><ns2:actTime>20100503165804<"));
-            assertFalse(responseXml.contains("actResult"));
-            assertTrue(responseXml.contains("referral><ns2:referralId>9871961"));
-            assertTrue(responseXml.contains("referralId><ns2:referralReason>Önskad undersökning  test Anamnes, status: test"));
-            assertTrue(responseXml.contains("referralReason><ns2:referralTime>20100503165805<"));
-            assertTrue(responseXml.contains("</ns2:patientId><ns2:accountableHealthcareProfessional><ns2:authorTime>20100503165801</ns2:authorTime>")); //20100503165801
-            assertTrue(responseXml.contains("healthcareProfessionalHSAId><ns2:healthcareProfessionalName>Jarl Sternum<"));
-            // 
-        } catch (XMLStreamException e) {
-            fail(e.getLocalizedMessage());
-        } catch (MapperException e) {
-            fail(e.getLocalizedMessage());
-        }
+        assertTrue(responseXml.contains("<GetReferralOutcomeResponse"));
+        assertTrue(responseXml.contains("sourceSystemHSAId><ns2:documentTime>20100503165801</ns2:documentTime><ns2:patientId>"));
+        assertTrue(responseXml.contains("referralOutcomeTypeCode>SS"));
+        assertTrue(responseXml.contains("referralOutcomeTitle>Allmän Remiss Vårdcentralen Strängnäs EDI"));
+        assertTrue(responseXml.contains("referralOutcomeText>Svar: test Svarsdatum: 100503 Dikterande"));
+        assertTrue(responseXml.contains("act><ns2:actCode><ns2:code>620<"));
+        assertTrue(responseXml.contains("<ns2:codeSystem>1.2.752.129.2.2.2.1<"));
+        assertTrue(responseXml.contains("<ns2:actText>Önskad undersökning: test"));
+        assertTrue(responseXml.contains("actText><ns2:actTime>20100503165804<"));
+        assertFalse(responseXml.contains("actResult"));
+        assertTrue(responseXml.contains("referral><ns2:referralId>9871961"));
+        assertTrue(responseXml.contains("referralId><ns2:referralReason>Önskad undersökning  test Anamnes, status: test"));
+        assertTrue(responseXml.contains("referralReason><ns2:referralTime>20100503165805<"));
+        assertTrue(responseXml.contains("</ns2:patientId><ns2:accountableHealthcareProfessional><ns2:authorTime>20100503165801</ns2:authorTime>")); //20100503165801
+        assertTrue(responseXml.contains("healthcareProfessionalHSAId><ns2:healthcareProfessionalName>Jarl Sternum<"));
+        // 
     }
     
     
     
     @Test
     public void mapMultipleResponses() {
-
-        ReferralOutcomeMapper objectUnderTest = getReferralOutcomeMapper();
-
-        // load xml from test file - this contains an <ehr_extract/>
-        StringBuilder xml13606Response = new StringBuilder();
-        try (@SuppressWarnings("resource") Scanner inputStringScanner = new Scanner(getClass().getResourceAsStream(Util.REFERRALOUTCOME_TEST_FILE_2), "UTF-8").useDelimiter("\\z")) {
-            while (inputStringScanner.hasNext()) {
-                xml13606Response.append(inputStringScanner.next());
-            }
-        }
-
-        // wrap the <ehr_extract/> in a <RIV13606REQUEST_EHR_EXTRACT_response/>
-        // opening tag
-        xml13606Response.insert("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".length(),"<RIV13606REQUEST_EHR_EXTRACT_response xmlns=\"urn:riv13606:v1.1\">");
-        // closing tag
-        xml13606Response.append("</RIV13606REQUEST_EHR_EXTRACT_response>\n");
         
-        // pass the <RIV13606REQUEST_EHR_EXTRACT_response/> message into the ReferralOutcomeMapper - expect back a <GetReferralOutcomeResponse/>
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-        Reader xmlReader = new StringReader(xml13606Response.toString());
-        XMLStreamReader xmlStreamReader;
-        try {
-            xmlStreamReader = xmlInputFactory.createXMLStreamReader(xmlReader);
-
-            MuleMessage mockMuleMessage = mock(MuleMessage.class);
-            when(mockMuleMessage.getPayload()).thenReturn(xmlStreamReader);
-            // argumentCaptor will capture the converted xml
-            ArgumentCaptor<Object> argumentCaptor = ArgumentCaptor.forClass(Object.class);
-
-            // method being exercised
-            objectUnderTest.mapResponse(mockMuleMessage);
-
+        String responseXml = getRivtaXml(getReferralOutcomeMapper(), Util.REFERRALOUTCOME_TEST_FILE_2);
             
-            // verifications & assertions
-            verify(mockMuleMessage).setPayload(argumentCaptor.capture());
-            String responseXml = (String)argumentCaptor.getValue();
-            log.debug(responseXml);
-            log.debug("");
-            
-            int occurrences = 0;
-            Pattern p = Pattern.compile("referralOutcomeBody");
-            Matcher m = p.matcher(responseXml);
-            while (m.find()) {
-                occurrences++;
-            }
-            assertEquals(6,occurrences);
-            
-            assertTrue(responseXml.contains("<GetReferralOutcomeResponse"));
-            assertTrue(responseXml.contains("referralOutcomeTypeCode>SS"));
-            
-        } catch (XMLStreamException e) {
-            fail(e.getLocalizedMessage());
-        } catch (MapperException e) {
-            fail(e.getLocalizedMessage());
+        int occurrences = 0;
+        Pattern p = Pattern.compile("referralOutcomeBody");
+        Matcher m = p.matcher(responseXml);
+        while (m.find()) {
+            occurrences++;
         }
+        assertEquals(6,occurrences);
+        
+        assertTrue(responseXml.contains("<GetReferralOutcomeResponse"));
+        assertTrue(responseXml.contains("referralOutcomeTypeCode>SS"));
     }
     
     
     @Test
     public void mapBlekinge() {
 
-        ReferralOutcomeMapper objectUnderTest = getReferralOutcomeMapper();
-
-        // load xml from test file - this contains an <ehr_extract/>
-        StringBuilder xml13606Response = new StringBuilder();
-        try (@SuppressWarnings("resource") Scanner inputStringScanner = new Scanner(getClass().getResourceAsStream(Util.REFERRALOUTCOME_TEST_FILE_3), "UTF-8").useDelimiter("\\z")) {
-            while (inputStringScanner.hasNext()) {
-                xml13606Response.append(inputStringScanner.next());
-            }
+        String responseXml = getRivtaXml(getReferralOutcomeMapper(), Util.REFERRALOUTCOME_TEST_FILE_3);
+            
+        int occurrences = 0;
+        Pattern p = Pattern.compile("referralOutcomeBody");
+        Matcher m = p.matcher(responseXml);
+        while (m.find()) {
+            occurrences++;
         }
-
-        // wrap the <ehr_extract/> in a <RIV13606REQUEST_EHR_EXTRACT_response/>
-        // opening tag
-        xml13606Response.insert("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".length(),"<RIV13606REQUEST_EHR_EXTRACT_response xmlns=\"urn:riv13606:v1.1\">");
-        // closing tag
-        xml13606Response.append("</RIV13606REQUEST_EHR_EXTRACT_response>\n");
+        assertEquals(6,occurrences);
         
-        // pass the <RIV13606REQUEST_EHR_EXTRACT_response/> message into the ReferralOutcomeMapper 
-        // expect back a <GetReferralOutcomeResponse/>
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-        Reader xmlReader = new StringReader(xml13606Response.toString());
-        XMLStreamReader xmlStreamReader;
-        try {
-            xmlStreamReader = xmlInputFactory.createXMLStreamReader(xmlReader);
-
-            MuleMessage mockMuleMessage = mock(MuleMessage.class);
-            when(mockMuleMessage.getPayload()).thenReturn(xmlStreamReader);
-            // argumentCaptor will capture the converted xml
-            ArgumentCaptor<Object> argumentCaptor = ArgumentCaptor.forClass(Object.class);
-
-            // method being exercised
-            objectUnderTest.mapResponse(mockMuleMessage);
-            
-            // verifications & assertions
-            verify(mockMuleMessage).setPayload(argumentCaptor.capture());
-            String responseXml = (String)argumentCaptor.getValue();
-            log.debug(responseXml);
-            log.debug("");
-            
-            int occurrences = 0;
-            Pattern p = Pattern.compile("referralOutcomeBody");
-            Matcher m = p.matcher(responseXml);
-            while (m.find()) {
-                occurrences++;
-            }
-            assertEquals(6,occurrences);
-            
-            assertTrue(responseXml.contains("<GetReferralOutcomeResponse"));
-            assertTrue(responseXml.contains("referralOutcomeTypeCode>SS"));
-            assertTrue(responseXml.contains("<ns2:referralAuthor><ns2:authorTime>20150703114102</ns2:authorTime><ns2:healthcareProfessionalHSAId>MO0775</ns2:healthcareProfessionalHSAId><ns2:healthcareProfessionalName>Monica Burman"));
-            assertFalse(responseXml.contains("</ns2:healthcareProfessionalCareGiverHSAId></ns2:referralAuthor>"));
-            
-        } catch (XMLStreamException e) {
-            fail(e.getLocalizedMessage());
-        } catch (MapperException e) {
-            fail(e.getLocalizedMessage());
-        }
+        assertTrue(responseXml.contains("<GetReferralOutcomeResponse"));
+        assertTrue(responseXml.contains("referralOutcomeTypeCode>SS"));
+        assertTrue(responseXml.contains("<ns2:referralAuthor><ns2:authorTime>20150703114102</ns2:authorTime><ns2:healthcareProfessionalHSAId>MO0775</ns2:healthcareProfessionalHSAId><ns2:healthcareProfessionalName>Monica Burman"));
+        assertFalse(responseXml.contains("</ns2:healthcareProfessionalCareGiverHSAId></ns2:referralAuthor>"));
     }
-    
     
 }
 
