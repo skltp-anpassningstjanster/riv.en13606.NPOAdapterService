0f507e0b795a59cc41b720ab698dd943ae69cd25
Martin Flower <martin.flower@callistaenterprise.se>
2015-10-21 13:51:56 +0200

    SERVICE-377 - Update gatling tests - work in progress

--- test/non-functional/gatling/src/test/scala/se/skl/skltpservices/npoadapter/scenarios/GetCareDocumentationScenario.scala
+++ test/non-functional/gatling/src/test/scala/se/skl/skltpservices/npoadapter/scenarios/GetCareDocumentationScenario.scala
@@ -8,6 +8,6 @@ object GetCareDocumentationScenario extends {
   val relativeUrl     = "getcaredocumentation/v2"
   val regex1          = "<ns2:healthcareProfessionalHSAId>SE2321000164-7381037590003ollvgiv-1"
   val regex2          = "Body><GetCareDocumentationResponse"
-  val minLength       = 13174
+  val minLength       = 10000
   
 } with AbstractGetRequest
--- test/non-functional/gatling/src/test/scala/se/skl/skltpservices/npoadapter/scenarios/GetMedicationHistoryScenario.scala
+++ test/non-functional/gatling/src/test/scala/se/skl/skltpservices/npoadapter/scenarios/GetMedicationHistoryScenario.scala
@@ -6,8 +6,8 @@ object GetMedicationHistoryScenario extends {
   val urn             = "urn:riv:clinicalprocess:activityprescription:actoutcome:GetMedicationHistoryResponder:1:GetMedicationHistory"
   val relativeUrl     = "getmedicationhistory/v2"
   val requestFileName = "GetMedicationHistoryRequest.xml"
-  val regex1          = "<ns2:documentId>SE1623210002198208149297ordination111"
+  val regex1          = "healthcareProfessionalCareGiverHSAId"
   val regex2          = "Body><GetMedicationHistoryResponse"
-  val minLength       = 12631
+  val minLength       = 10000
   
 } with AbstractGetRequest
