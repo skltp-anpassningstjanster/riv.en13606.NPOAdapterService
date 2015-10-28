e88268393285d6b577815ed120ff76ed2c9e5d9e
Martin Flower <martin.flower@callistaenterprise.se>
2015-10-28 12:54:56 +0100

    SERVICE-377 - Gatling test TP99 Step

---
+++ test/non-functional/gatling/src/test/scala/se/skl/skltpservices/npoadapter/TP99Step.scala
@@ -0,0 +1,40 @@
+package se.skl.skltpservices.npoadapter;
+
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import se.skl.skltpservices.npoadapter.scenarios.GetAlertInformationScenario
+import se.skl.skltpservices.npoadapter.scenarios.GetCareContactsScenario
+import se.skl.skltpservices.npoadapter.scenarios.GetCareDocumentationScenario
+import se.skl.skltpservices.npoadapter.scenarios.GetDiagnosisScenario
+import se.skl.skltpservices.npoadapter.scenarios.GetImagingOutcomeScenario 
+import se.skl.skltpservices.npoadapter.scenarios.GetLaboratoryOrderOutcomeScenario
+import se.skl.skltpservices.npoadapter.scenarios.GetMedicationHistoryScenario
+import se.skl.skltpservices.npoadapter.scenarios.GetReferralOutcomeScenario
+
+/**
+ * Exercise the Adapter at 50% CPU for a week.
+ * Test for memory leaks in the Adapter JVM.
+ * Test for degraded performance over time.
+ */
+class TP99Step extends Simulation with HasBaseURL {
+
+  // Relationship between requests per second and CPU 
+  
+  val newUsersPerSecond:Int     = 90
+  val testDuration              =  1 minutes
+    
+  val getParallel = scenario("TP99Step").
+                      uniformRandomSwitch(
+                        exec(GetAlertInformationScenario.request),
+                        exec(GetCareContactsScenario.request),
+                        exec(GetCareDocumentationScenario.request),
+                        exec(GetDiagnosisScenario.request),
+                        exec(GetImagingOutcomeScenario.request),
+                        exec(GetLaboratoryOrderOutcomeScenario.request),
+                        exec(GetMedicationHistoryScenario.request),
+                        exec(GetReferralOutcomeScenario.request)
+                     )
+  setUp(getParallel.inject(constantUsersPerSec(newUsersPerSecond) during(testDuration)) 
+                   .protocols(http.baseURL(baseURL)))
+}
